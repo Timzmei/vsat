68e26983d12e997edf6c461b8fdfa30159f1e8f5
Timur Guliev <mail@guliev-timur.ru>
2023-02-12 17:14:42 +0500

    отображение списка станций посточно из массива

--- src/main/java/ru/rtrn/vsat/controllers/VsatController.java
+++ src/main/java/ru/rtrn/vsat/controllers/VsatController.java
@@ -3,20 +3,22 @@ package ru.rtrn.vsat.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.rtrn.vsat.entities.Station;
 import ru.rtrn.vsat.httpReq.SimpleRequest;
 import ru.rtrn.vsat.services.SnmpSevice;
 import ru.rtrn.vsat.services.StationService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
+@RequestMapping("stations")
 public class VsatController {
     @Autowired
     private SimpleRequest simpleRequest;
@@ -25,6 +27,59 @@ public class VsatController {
     @Autowired
     private StationService stationService;
 
+//    private int counter = 4;
+//
+//
+//    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+//        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
+//        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
+//        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
+//    }};
+//
+//    @GetMapping
+//    public List<Map<String, String>> list() {
+//        return messages;
+//    }
+//
+//
+//    @GetMapping("{id}")
+//    public Map<String, String> getOne(@PathVariable String id) {
+//        return getMessage(id);
+//    }
+//
+//    private Map<String, String> getMessage(@PathVariable String id) {
+//        return messages.stream()
+//                .filter(message -> message.get("id").equals(id)).findFirst().get();
+//    }
+//
+//    @PostMapping
+//    public Map<String, String> create(@RequestBody Map<String, String> message) {
+//        message.put("id", String.valueOf(counter++));
+//
+//        messages.add(message);
+//
+//        return message;
+//    }
+//
+//    @PutMapping("{id}")
+//    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+//        Map<String, String> messageFromDb = getMessage(id);
+//
+//        messageFromDb.putAll(message);
+//        messageFromDb.put("id", id);
+//
+//        return messageFromDb;
+//    }
+//
+//    @DeleteMapping("{id}")
+//    public void delete(@PathVariable String id) {
+//        Map<String, String> message = getMessage(id);
+//
+//        messages.remove(message);
+//    }
+
+
+    @GetMapping
     public List<Station> list() {
         return stationService.getStations();
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,16 +6,26 @@ var app = new Vue({
 });
 
 
-var messageApi = Vue.resource('/{/id}')
+var messageApi = Vue.resource('/stations{/id}')
 
 Vue.component('message-row', {
   props: ['message'],
-  template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+  template: '<div><i>({{ message.id }})</i> {{ message.name }} {{ message.ip }} {{ message.value }} {{ message.rele }} {{ message.status }}</div>'
 });
 
 Vue.component('messages-list', {
   props: ['messages'],
-  template: '<div><message-row v-for="message in messages" v-bind:key="message.id" :message="message" /></div>'
+  template: '<div><message-row v-for="message in messages" v-bind:key="message.id" :message="message" /></div>',
+  created: function() {
+    messageApi.get().then(result =>
+        {
+            console.log(result)
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        }
+    )
+  }
 })
 
 var app7 = new Vue({
@@ -24,4 +34,4 @@ var app7 = new Vue({
   data: {
     messages: []
   }
-})
+});
